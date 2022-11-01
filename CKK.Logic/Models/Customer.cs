b8cc3a796d45eb71f8bac3627010f421e4679880
4400114616 <drschokolade1@gmail.com>
2022-11-01 11:42:25 -0600

    commit

---
+++ CKK.Logic/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class Customer
+    {
+        private int _id;
+        private string _name;
+        private string _address;
+
+       
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int _id) { }
+
+
+
+        
+           
+        
+
+    }
+}
